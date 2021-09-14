f4584d98dac18285ceede44b3b0bab0909ee313a
hicham <hicham.boushaba@gmail.com>
2021-09-14 14:11:58 +0100

    First implementation of product's loading with pagination

--- app/src/main/java/com/hicham/wcstoreapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/home/HomeScreen.kt
@@ -1,47 +1,41 @@
 package com.hicham.wcstoreapp.ui.home
 
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.accompanist.insets.navigationBarsHeight
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
 import com.hicham.wcstoreapp.R
-import com.hicham.wcstoreapp.data.Status
-import com.hicham.wcstoreapp.data.source.ProductsRepository
-import com.hicham.wcstoreapp.data.source.network.FakeWooCommerceApi
+import com.hicham.wcstoreapp.data.source.fake.FakeProductsRepository
+import com.hicham.wcstoreapp.models.Product
 import com.hicham.wcstoreapp.ui.components.InsetAwareTopAppBar
 import com.hicham.wcstoreapp.ui.theme.WCStoreAppTheme
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
+import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = viewModel(),
     scaffoldState: ScaffoldState = rememberScaffoldState()
 ) {
-    val state by viewModel.uiState.collectAsState()
-
-    HomeScreen(uiState = state, scaffoldState = scaffoldState)
+    HomeScreen(productsFlow = viewModel.products, scaffoldState = scaffoldState)
 }
 
 @Composable
 fun HomeScreen(
-    uiState: UiState,
+    productsFlow: Flow<PagingData<Product>>,
     scaffoldState: ScaffoldState
 ) {
+    val lazyProductList = productsFlow.collectAsLazyPagingItems()
+
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
@@ -52,8 +46,8 @@ fun HomeScreen(
         }
     ) {
         LazyColumn {
-            items(uiState.products) { product ->
-                ProductCard(product = product)
+            items(items = lazyProductList) { product ->
+                product?.let { ProductCard(product = it) }
             }
         }
     }
@@ -62,17 +56,11 @@ fun HomeScreen(
 @Preview
 @Composable
 fun DefaultHome() {
-    val products = runBlocking {
-            ProductsRepository(FakeWooCommerceApi(Json)).getProductList()
-                .first().getOrThrow()
-        }
+    val productsFlow = FakeProductsRepository().getProductList()
 
     WCStoreAppTheme {
         HomeScreen(
-            UiState(
-                isLoading = false,
-                products = products
-            ),
+            productsFlow = productsFlow,
             scaffoldState = rememberScaffoldState()
         )
     }
--- app/src/test/java/com/hicham/wcstoreapp/ExampleUnitTest.kt
+++ app/src/test/java/com/hicham/wcstoreapp/ExampleUnitTest.kt
@@ -1,5 +1,14 @@
 package com.hicham.wcstoreapp
 
+import com.hicham.wcstoreapp.data.Status
+import com.hicham.wcstoreapp.data.source.ProductsRepository
+import com.hicham.wcstoreapp.data.source.network.FakeWooCommerceApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +21,13 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val repository = ProductsRepository(FakeWooCommerceApi(Json))
+
+        runBlocking {
+            println(repository.getProductList()
+                .filter { it.status == Status.SUCCESS }
+                .first().status)
+
+        }
     }
 }
