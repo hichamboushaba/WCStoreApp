bab73856ace67a307ef495184e236428feb54fb5
hicham <hicham.boushaba@outlook.com>
2021-09-18 15:49:49 +0100

    Rename class

--- app/src/main/java/com/hicham/wcstoreapp/data/CartRepository.kt
+++ app/src/main/java/com/hicham/wcstoreapp/data/CartRepository.kt
@@ -3,7 +3,7 @@ package com.hicham.wcstoreapp.data
 import com.hicham.wcstoreapp.models.Product
 import kotlinx.coroutines.flow.StateFlow
 
-interface Cart {
+interface CartRepository {
     val items: StateFlow<List<Product>>
     fun addItem(product: Product)
     fun deleteItem(product: Product)
