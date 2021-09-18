15673fd9684ed8387d1d41daf55112f980968488
hicham <hicham.boushaba@gmail.com>
2021-09-18 15:50:39 +0100

    Make items a regular Flow

--- app/src/main/java/com/hicham/wcstoreapp/data/CartRepository.kt
+++ app/src/main/java/com/hicham/wcstoreapp/data/CartRepository.kt
@@ -1,10 +1,10 @@
 package com.hicham.wcstoreapp.data
 
 import com.hicham.wcstoreapp.models.Product
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
 
 interface CartRepository {
-    val items: StateFlow<List<Product>>
+    val items: Flow<List<Product>>
     fun addItem(product: Product)
     fun deleteItem(product: Product)
     fun clearProduct(product: Product)
