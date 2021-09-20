61411e13c551af8d42f9c4b36e82f95f136dd91e
hicham <hicham.boushaba@outlook.com>
2021-09-20 14:51:03 +0100

    Extract the topbar to a reusable component

--- app/src/main/java/com/hicham/wcstoreapp/ui/cart/CartScreen.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/cart/CartScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -18,7 +16,7 @@ import com.hicham.wcstoreapp.data.source.fake.PRODUCTS_JSON
 import com.hicham.wcstoreapp.data.source.network.NetworkProduct
 import com.hicham.wcstoreapp.models.Product
 import com.hicham.wcstoreapp.models.toProduct
-import com.hicham.wcstoreapp.ui.components.InsetAwareTopAppBar
+import com.hicham.wcstoreapp.ui.components.WCTopAppBar
 import compose.icons.TablerIcons
 import compose.icons.tablericons.ShoppingCart
 import compose.icons.tablericons.ShoppingCartPlus
@@ -51,7 +49,22 @@ fun CartScreen(
 ) {
     Scaffold(
         topBar = {
-            TopBar(onBack)
+            WCTopAppBar(
+                title = {
+                    Image(
+                        imageVector = TablerIcons.ShoppingCart,
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "Shopping Cart",
+                        maxLines = 1,
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                    )
+                },
+                onNavigationClick = onBack
+            )
         }
     ) { innerPadding ->
         if (items.isEmpty()) {
@@ -97,44 +110,6 @@ fun CartScreen(
         }
     }
 }
-
-@Composable
-private fun TopBar(onBack: () -> Unit) {
-    InsetAwareTopAppBar(
-        title = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentWidth(align = Alignment.CenterHorizontally)
-                    .padding(end = 48.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    imageVector = TablerIcons.ShoppingCart,
-                    contentDescription = null
-                )
-                Text(
-                    text = "Shopping Cart",
-                    maxLines = 1,
-                    style = MaterialTheme.typography.h6,
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp)
-                )
-            }
-        },
-        navigationIcon = {
-            IconButton(onClick = onBack) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBack,
-                    contentDescription = "",
-                    tint = MaterialTheme.colors.primary
-                )
-            }
-        },
-        backgroundColor = MaterialTheme.colors.surface
-    )
-}
-
 
 @Preview
 @Composable
