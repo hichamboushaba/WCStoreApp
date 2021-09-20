d7af93c3b6d0b33b40c9e909a04d03732f8df228
hicham <hicham.boushaba@outlook.com>
2021-09-20 19:17:54 +0100

    Initial implemenation of Product screen

--- app/src/main/java/com/hicham/wcstoreapp/ui/components/WCTopAppBar.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/components/WCTopAppBar.kt
@@ -4,12 +4,39 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun WCTopAppBar(
+    title: String,
+    navigationIcon: @Composable () -> Unit = {
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "",
+            tint = MaterialTheme.colors.primary
+        )
+    },
+    onNavigationClick: () -> Unit,
+) {
+    WCTopAppBar(
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                style = MaterialTheme.typography.h6
+            )
+        },
+        navigationIcon = navigationIcon,
+        onNavigationClick = onNavigationClick
+    )
+}
 
 @Composable
 fun WCTopAppBar(
--- app/src/main/java/com/hicham/wcstoreapp/ui/product/ProductViewModel.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/product/ProductViewModel.kt
@@ -2,15 +2,60 @@ package com.hicham.wcstoreapp.ui.product
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hicham.wcstoreapp.data.CartRepository
+import com.hicham.wcstoreapp.data.CurrencyFormatProvider
+import com.hicham.wcstoreapp.data.ProductsRepository
+import com.hicham.wcstoreapp.models.Product
+import com.hicham.wcstoreapp.ui.CurrencyFormatter
 import com.hicham.wcstoreapp.ui.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
-class ProductViewModel @Inject constructor(savedStateHandle: SavedStateHandle) : ViewModel() {
-    private val productId = savedStateHandle.get<Long>(Screen.Product.productIdKey)
+class ProductViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val productsRepository: ProductsRepository,
+    private val currencyFormatProvider: CurrencyFormatProvider,
+    private val cartRepository: CartRepository
+) : ViewModel() {
+    private val productId = savedStateHandle.get<Long>(Screen.Product.productIdKey)!!
+
+    private val _uiState = MutableStateFlow<UiState>(UiState.LoadingState)
+    val uiState = _uiState.asStateFlow()
 
     init {
-        println("productId -> $productId")
+        val productFlow = flow { emit(productsRepository.getProduct(productId)) }
+        val cartQuantityFlow = cartRepository.items.map { it.filter { it.id == productId }.size }
+        combine(
+            productFlow,
+            cartQuantityFlow,
+            currencyFormatProvider.formatSettings
+        ) { product, cartQuantity, formatSettings ->
+            Triple(product, cartQuantity, CurrencyFormatter(formatSettings))
+        }
+            .onEach { (product, cartQuantity, currencyFormatter) ->
+                _uiState.value = UiState.SuccessState(
+                    product = product,
+                    priceFormatted = currencyFormatter.format(product.price),
+                    quantityInCart = cartQuantity
+                )
+            }
+            .catch {
+                _uiState.value = UiState.ErrorState
+            }
+            .launchIn(viewModelScope)
+    }
+
+    sealed class UiState {
+        object LoadingState : UiState()
+        data class SuccessState(
+            val product: Product,
+            val priceFormatted: String,
+            val quantityInCart: Int
+        ) : UiState()
+
+        object ErrorState : UiState()
     }
 }
