1f111feb175f9fefcf14a6fe58f23370479cd0e8
hicham <hicham.boushaba@outlook.com>
2021-09-22 14:20:41 +0100

    Handle more advanced navigation events

--- app/src/main/java/com/hicham/wcstoreapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/main/MainActivity.kt
@@ -19,23 +19,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.insets.ui.BottomNavigation
-import com.hicham.wcstoreapp.ui.NavigationCommand
-import com.hicham.wcstoreapp.ui.NavigationManager
+import com.hicham.wcstoreapp.ui.navigation.NavigationCommand
+import com.hicham.wcstoreapp.ui.navigation.NavigationManager
 import com.hicham.wcstoreapp.ui.navigation.Screen
-import com.hicham.wcstoreapp.ui.cart.CartScreen
-import com.hicham.wcstoreapp.ui.home.HomeScreen
 import com.hicham.wcstoreapp.ui.navigation.MainNavGraph
-import com.hicham.wcstoreapp.ui.product.ProductScreen
 import com.hicham.wcstoreapp.ui.theme.WCStoreAppTheme
 import compose.icons.TablerIcons
 import compose.icons.tablericons.ShoppingCart
@@ -80,8 +74,12 @@ private fun Main(uiState: MainViewModel.UiState, navigationManager: NavigationMa
     LaunchedEffect(Unit) {
         navigationManager.navigationCommands.collect {
             when (it) {
-                is NavigationCommand.NavigateToRoute -> navController.navigate(it.route)
+                is NavigationCommand.NavigateToRoute -> navController.navigate(it.route, it.options)
                 NavigationCommand.NavigateUp -> navController.navigateUp()
+                is NavigationCommand.PopUpToRoute -> navController.popBackStack(
+                    it.route,
+                    it.inclusive
+                )
             }
         }
     }
