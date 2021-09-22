71033d109b2be3d20a288f12639c0dff8ad6825c
hicham <hicham.boushaba@gmail.com>
2021-09-22 14:20:41 +0100

    Handle more advanced navigation events

--- app/src/main/java/com/hicham/wcstoreapp/ui/navigation/NavigationManager.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/navigation/NavigationManager.kt
@@ -1,5 +1,8 @@
-package com.hicham.wcstoreapp.ui
+package com.hicham.wcstoreapp.ui.navigation
 
+import androidx.navigation.NavOptions
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.navOptions
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -19,12 +22,24 @@ class NavigationManager @Inject constructor() {
         _navigationCommands.tryEmit(NavigationCommand.NavigateToRoute(route))
     }
 
+    fun navigate(route: String, optionsBuilder: NavOptionsBuilder.() -> Unit) {
+        val options = navOptions(optionsBuilder)
+        _navigationCommands.tryEmit(NavigationCommand.NavigateToRoute(route, options))
+    }
+
     fun navigateUp() {
         _navigationCommands.tryEmit(NavigationCommand.NavigateUp)
+    }
+
+    fun popUpTo(route: String, inclusive: Boolean = false) {
+        _navigationCommands.tryEmit(NavigationCommand.PopUpToRoute(route, inclusive))
     }
 }
 
 sealed class NavigationCommand {
     object NavigateUp : NavigationCommand()
-    data class NavigateToRoute(val route: String) : NavigationCommand()
+    data class NavigateToRoute(val route: String, val options: NavOptions? = null) :
+        NavigationCommand()
+
+    data class PopUpToRoute(val route: String, val inclusive: Boolean) : NavigationCommand()
 }
