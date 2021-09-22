30ffe8f0562d6d396377e383533b225c0d107980
hicham <hicham.boushaba@gmail.com>
2021-09-22 14:52:23 +0100

    Fix imports

--- app/src/main/java/com/hicham/wcstoreapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/home/HomeViewModel.kt
@@ -9,7 +9,7 @@ import com.hicham.wcstoreapp.data.CurrencyFormatProvider
 import com.hicham.wcstoreapp.data.ProductsRepository
 import com.hicham.wcstoreapp.models.Product
 import com.hicham.wcstoreapp.ui.CurrencyFormatter
-import com.hicham.wcstoreapp.ui.NavigationManager
+import com.hicham.wcstoreapp.ui.navigation.NavigationManager
 import com.hicham.wcstoreapp.ui.navigation.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.combine
