4ab9315258113b48ca05747380ea66e1b99df8fc
hicham <hicham.boushaba@gmail.com>
2021-09-25 11:38:51 +0100

    Use Room's relation for combining the cart items with products

--- app/src/main/java/com/hicham/wcstoreapp/data/source/db/daos/CartDao.kt
+++ app/src/main/java/com/hicham/wcstoreapp/data/source/db/daos/CartDao.kt
@@ -2,22 +2,20 @@ package com.hicham.wcstoreapp.data.source.db.daos
 
 import androidx.room.*
 import com.hicham.wcstoreapp.data.source.db.entities.CartItemEntity
+import com.hicham.wcstoreapp.data.source.db.entities.CartItemWithProduct
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CartDao {
     @Transaction
     @Query("SELECT * FROM CartItemEntity")
-    fun getCartItems(): Flow<List<CartItemEntity>>
+    fun getCartItems(): Flow<List<CartItemWithProduct>>
 
     @Query("SELECT * FROM CartItemEntity WHERE productId = :productId")
     suspend fun getCartItem(productId: Long): CartItemEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertItem(vararg cartItemEntity: CartItemEntity)
-
-    @Delete
-    suspend fun deleteItem(cartItemEntity: CartItemEntity)
 
     @Query("DELETE FROM CartItemEntity WHERE productId = :productId")
     suspend fun deleteCartItemForProductId(productId: Long)
