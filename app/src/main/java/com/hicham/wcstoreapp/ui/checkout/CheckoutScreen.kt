4a9637c54ff66f436eb4128298b4f903a45c064f
hicham <hicham.boushaba@outlook.com>
2021-09-26 13:04:21 +0100

    Add screen and viewmodel files for checkout

---
+++ app/src/main/java/com/hicham/wcstoreapp/ui/checkout/CheckoutScreen.kt
@@ -0,0 +1,20 @@
+package com.hicham.wcstoreapp.ui.checkout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CheckoutScreen(viewModel: CheckoutViewModel) {
+
+}
+
+@Composable
+private fun CheckoutScreen() {
+
+}
+
+@Preview
+@Composable
+private fun CheckoutPreview() {
+    CheckoutScreen()
+}
---
+++ app/src/main/java/com/hicham/wcstoreapp/ui/checkout/CheckoutViewModel.kt
@@ -0,0 +1,8 @@
+package com.hicham.wcstoreapp.ui.checkout
+
+import com.hicham.wcstoreapp.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class CheckoutViewModel : BaseViewModel() {
+}
