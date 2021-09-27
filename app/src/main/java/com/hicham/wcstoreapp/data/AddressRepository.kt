ad347d71e8973fd9cdd3edae3f9cdd49551a3b06
hicham <hicham.boushaba@outlook.com>
2021-09-27 20:20:06 +0100

    Add address data infrastructure

---
+++ app/src/main/java/com/hicham/wcstoreapp/data/AddressRepository.kt
@@ -0,0 +1,15 @@
+package com.hicham.wcstoreapp.data
+
+import com.hicham.wcstoreapp.models.Address
+import kotlinx.coroutines.flow.Flow
+
+interface AddressRepository {
+    val savedAddresses: Flow<List<Address>>
+    val primaryShippingAddress: Flow<Address?>
+    val primaryBillingAddress: Flow<Address?>
+
+    suspend fun addAddress(address: Address)
+    suspend fun removeAddress(address: Address)
+    suspend fun setPrimaryShippingAddress(address: Address)
+    suspend fun setPrimaryBillingAddress(address: Address)
+}
---
+++ app/src/main/java/com/hicham/wcstoreapp/data/source/db/entities/AddressEntity.kt
@@ -0,0 +1,35 @@
+package com.hicham.wcstoreapp.data.source.db.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hicham.wcstoreapp.models.Address
+
+@Entity
+data class AddressEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val firstName: String,
+    val lastName: String,
+    val street1: String,
+    val street2: String?,
+    val phone: String?,
+    val city: String,
+    val state: String?,
+    val postCode: String,
+    val country: String
+) {
+    fun toAddress() = Address(
+        firstName = firstName,
+        lastName = lastName,
+        street1 = street1,
+        street2 = street2,
+        phone = phone,
+        city = city,
+        state = state,
+        postCode = postCode,
+        country = country
+    )
+
+    fun isSameAsAddress(address: Address): Boolean {
+        return toAddress() == address
+    }
+}
