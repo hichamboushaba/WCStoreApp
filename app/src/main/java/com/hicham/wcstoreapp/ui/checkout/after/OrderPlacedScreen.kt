314bfab8e4a8a6d0713148c286099e0661cc90f4
hicham <hicham.boushaba@outlook.com>
2021-09-28 12:53:31 +0100

    Add a button to close order screen

--- app/src/main/java/com/hicham/wcstoreapp/ui/checkout/after/OrderPlacedScreen.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/checkout/after/OrderPlacedScreen.kt
@@ -3,7 +3,9 @@ package com.hicham.wcstoreapp.ui.checkout.after
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -14,11 +16,13 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.hicham.wcstoreapp.R
 import compose.icons.TablerIcons
 
 @Composable
 fun OrderPlacedScreen(orderId: Long) {
+    val navController = rememberNavController()
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
@@ -30,6 +34,12 @@ fun OrderPlacedScreen(orderId: Long) {
             contentDescription = ""
         )
         Text(text = "Order id is #$orderId")
+        Spacer(modifier = Modifier)
+        Button(onClick = {
+            navController.navigateUp()
+        }) {
+            Text(text = "Continue shopping")
+        }
     }
 }
 
