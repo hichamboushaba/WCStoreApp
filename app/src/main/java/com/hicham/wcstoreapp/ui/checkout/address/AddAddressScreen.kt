56ba187845372ebb747af767aad30de3db5a99bf
hicham <hicham.boushaba@gmail.com>
2021-10-02 14:57:48 +0100

    Add input validation for add address screen

--- app/src/main/java/com/hicham/wcstoreapp/ui/checkout/address/AddAddressScreen.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/checkout/address/AddAddressScreen.kt
@@ -2,19 +2,24 @@ package com.hicham.wcstoreapp.ui.checkout.address
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hicham.wcstoreapp.ui.common.InputField
+import com.hicham.wcstoreapp.ui.common.RequiredField
 import com.hicham.wcstoreapp.ui.common.components.ToolbarScreen
 import com.hicham.wcstoreapp.ui.theme.WCStoreAppTheme
+import compose.icons.TablerIcons
+import compose.icons.tablericons.InfoCircle
 
 @Composable
 fun AddAddressScreen(viewModel: AddAddressViewModel) {
@@ -37,127 +42,108 @@ private fun AddAddressScreen(
         Column(
             modifier = Modifier
                 .verticalScroll(scrollState)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+                .fillMaxSize()
         ) {
             Spacer(modifier = Modifier)
-            OutlinedTextField(
-                value = state.firstName.orEmpty(),
+            TextField(
+                inputField = state.firstName,
                 onValueChange = {
                     onFieldEdited(
                         AddAddressViewModel.Field.FirstName,
                         it
                     )
                 },
-                label = {
-                    Text("First Name")
-                },
+                label = "First Name",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-                colors = TextFieldDefaults.textFieldColors()
+                    .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.lastName.orEmpty(),
+            TextField(
+                inputField = state.lastName,
                 onValueChange = {
                     onFieldEdited(
                         AddAddressViewModel.Field.LastName,
                         it
                     )
                 },
-                label = {
-                    Text("Last Name")
-                },
+                label = "Last Name",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.phone.orEmpty(),
+            TextField(
+                inputField = state.phone,
                 onValueChange = { onFieldEdited(AddAddressViewModel.Field.Phone, it) },
-                label = {
-                    Text("Phone")
-                },
+                label = "Phone",
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.street1.orEmpty(),
+            TextField(
+                inputField = state.street1,
                 onValueChange = {
                     onFieldEdited(
                         AddAddressViewModel.Field.Street1,
                         it
                     )
                 },
-                label = {
-                    Text("Street 1")
-                },
+                label = "Street 1",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.street2.orEmpty(),
+            TextField(
+                inputField = state.street2,
                 onValueChange = {
                     onFieldEdited(
                         AddAddressViewModel.Field.Street2,
                         it
                     )
                 },
-                label = {
-                    Text("Street 2")
-                },
+                label = "Street 2",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.city.orEmpty(),
+            TextField(
+                inputField = state.city,
                 onValueChange = { onFieldEdited(AddAddressViewModel.Field.City, it) },
-                label = {
-                    Text("City")
-                },
+                label = "City",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.state.orEmpty(),
+            TextField(
+                inputField = state.state,
                 onValueChange = { onFieldEdited(AddAddressViewModel.Field.State, it) },
-                label = {
-                    Text("State")
-                },
+                label = "State",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.postCode.orEmpty(),
+            TextField(
+                inputField = state.postCode,
                 onValueChange = {
                     onFieldEdited(
                         AddAddressViewModel.Field.PostCode,
                         it
                     )
                 },
-                label = {
-                    Text("Postal Code")
-                },
+                label = "Postal Code",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             )
-            OutlinedTextField(
-                value = state.country.orEmpty(),
+            TextField(
+                inputField = state.country,
                 onValueChange = {
                     onFieldEdited(
                         AddAddressViewModel.Field.Country,
                         it
                     )
                 },
-                label = {
-                    Text("Country")
-                },
+                label = "Country",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
@@ -169,10 +155,51 @@ private fun AddAddressScreen(
                 onClick = onSaveClicked,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp),
+                enabled = state.areAllRequiredFieldsValid
             ) {
                 Text(text = "Save Address")
             }
+        }
+    }
+}
+
+@Composable
+private fun TextField(
+    label: String,
+    inputField: InputField<*>,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+) {
+    val isError = inputField.error != null
+    Column(modifier = modifier) {
+        OutlinedTextField(
+            value = inputField.content,
+            onValueChange = onValueChange,
+            isError = isError,
+            trailingIcon = {
+                if (isError) Icon(
+                    imageVector = TablerIcons.InfoCircle,
+                    contentDescription = "",
+                    tint = Color.Red
+                )
+            },
+            label = {
+                Text(label)
+            },
+            keyboardOptions = keyboardOptions,
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(),
+        )
+        if (isError) {
+            Text(
+                text = stringResource(id = inputField.error!!),
+                color = Color.Red,
+                modifier = Modifier.defaultMinSize(minHeight = 24.dp)
+            )
+        } else {
+            Spacer(modifier = Modifier.height(24.dp))
         }
     }
 }
@@ -181,6 +208,10 @@ private fun AddAddressScreen(
 @Composable
 private fun AddAddressPreview() {
     WCStoreAppTheme() {
-        AddAddressScreen(state = AddAddressViewModel.UiState())
+        AddAddressScreen(
+            state = AddAddressViewModel.UiState(
+                firstName = RequiredField("").validate()
+            )
+        )
     }
 }
--- app/src/main/java/com/hicham/wcstoreapp/ui/checkout/address/AddAddressViewModel.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/checkout/address/AddAddressViewModel.kt
@@ -1,15 +1,23 @@
 package com.hicham.wcstoreapp.ui.checkout.address
 
+import android.util.Patterns
+import androidx.core.util.PatternsCompat
 import androidx.lifecycle.viewModelScope
+import com.hicham.wcstoreapp.R
 import com.hicham.wcstoreapp.data.AddressRepository
 import com.hicham.wcstoreapp.models.Address
 import com.hicham.wcstoreapp.ui.BaseViewModel
+import com.hicham.wcstoreapp.ui.common.InputField
+import com.hicham.wcstoreapp.ui.common.OptionalField
+import com.hicham.wcstoreapp.ui.common.RequiredField
 import com.hicham.wcstoreapp.ui.navigation.NavigationManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,16 +42,16 @@ class AddAddressViewModel @Inject constructor(
         viewModelScope.launch {
             val address = uiState.value.let {
                 Address(
-                    label = "",
-                    it.firstName!!,
-                    it.lastName!!,
-                    it.street1!!,
-                    it.street2,
-                    it.phone,
-                    it.city!!,
-                    it.state,
-                    it.postCode!!,
-                    it.country!!,
+                    label = it.addressLabel.content,
+                    firstName = it.firstName.content,
+                    lastName = it.lastName.content,
+                    street1 = it.street1.content,
+                    street2 = it.street2.content,
+                    phone = it.phone.content,
+                    city = it.city.content,
+                    state = it.state.content,
+                    postCode = it.postCode.content,
+                    country = it.country.content
                 )
             }
             addressRepository.addAddress(address)
@@ -52,28 +60,60 @@ class AddAddressViewModel @Inject constructor(
     }
 
     data class UiState(
-        val firstName: String? = null,
-        val lastName: String? = null,
-        val street1: String? = null,
-        val street2: String? = null,
-        val phone: String? = null,
-        val city: String? = null,
-        val state: String? = null,
-        val postCode: String? = null,
-        val country: String? = null
+        val addressLabel: OptionalField = OptionalField(""),
+        val firstName: RequiredField = RequiredField(""),
+        val lastName: RequiredField = RequiredField(""),
+        val street1: RequiredField = RequiredField(""),
+        val street2: OptionalField = OptionalField(""),
+        val phone: PhoneField = PhoneField(""),
+        val city: RequiredField = RequiredField(""),
+        val state: OptionalField = OptionalField(""),
+        val postCode: RequiredField = RequiredField(""),
+        val country: RequiredField = RequiredField("")
     ) {
+        val areAllRequiredFieldsValid
+            get() = Field.values().all { get(it).isValid }
+
+        operator fun get(field: Field): InputField<*> {
+            return when (field) {
+                Field.FirstName -> firstName
+                Field.LastName -> lastName
+                Field.Street1 -> street1
+                Field.Street2 -> street2
+                Field.Phone -> phone
+                Field.City -> city
+                Field.State -> state
+                Field.PostCode -> postCode
+                Field.Country -> country
+            }
+        }
+
         fun updateField(field: Field, content: String): UiState {
             return when (field) {
-                Field.FirstName -> copy(firstName = content)
-                Field.LastName -> copy(lastName = content)
-                Field.Street1 -> copy(street1 = content)
-                Field.Street2 -> copy(street2 = content)
-                Field.Phone -> copy(phone = content)
-                Field.City -> copy(city = content)
-                Field.State -> copy(state = content)
-                Field.PostCode -> copy(postCode = content)
-                Field.Country -> copy(country = content)
+                Field.FirstName -> copy(firstName = firstName.copy(content = content).validate())
+                Field.LastName -> copy(lastName = lastName.copy(content = content).validate())
+                Field.Street1 -> copy(street1 = street1.copy(content = content).validate())
+                Field.Street2 -> copy(street2 = street2.copy(content = content).validate())
+                Field.Phone -> copy(phone = phone.copy(content = content).validate())
+                Field.City -> copy(city = city.copy(content = content).validate())
+                Field.State -> copy(state = state.copy(content = content).validate())
+                Field.PostCode -> copy(postCode = postCode.copy(content = content).validate())
+                Field.Country -> copy(country = country.copy(content = content).validate())
             }
+        }
+
+        fun validateInput() {
+            Field.values().forEach {
+                get(it).validate()
+            }
+        }
+    }
+
+    @Parcelize
+    data class PhoneField(override val content: String) : InputField<PhoneField>(content) {
+        override fun validateInternal(): Int? {
+            return if (content.isEmpty() || Patterns.PHONE.matcher(content).matches()) null
+            else R.string.error_invalid_phone
         }
     }
 
