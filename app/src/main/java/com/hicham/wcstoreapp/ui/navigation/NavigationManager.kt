11a86551e3c38cb94e14a1edcf69bdcf7ff8b440
hicham <hicham.boushaba@gmail.com>
2021-10-03 20:06:26 +0100

    Refactor navigation with result to use its own navigation command

--- app/src/main/java/com/hicham/wcstoreapp/ui/navigation/NavigationManager.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/navigation/NavigationManager.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.navOptions
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -16,36 +15,19 @@ class NavigationManager @Inject constructor() {
     // and still get the navigation result later
     private val navControllerFlow = MutableStateFlow<NavController?>(null)
 
+    private val navigationCommands =
+        MutableSharedFlow<NavigationCommand>(extraBufferCapacity = Int.MAX_VALUE)
+
+
     suspend fun handleNavigationCommands(navController: NavController) {
         navigationCommands
             .onSubscription { this@NavigationManager.navControllerFlow.value = navController }
             .onCompletion { this@NavigationManager.navControllerFlow.value = null }
-            .collect {
-                when (it) {
-                    is NavigationCommand.NavigateToRoute -> navController.navigate(
-                        it.route,
-                        it.options
-                    )
-                    NavigationCommand.NavigateUp -> navController.navigateUp()
-                    is NavigationCommand.PopUpToRoute -> navController.popBackStack(
-                        it.route,
-                        it.inclusive
-                    )
-                }
-            }
+            .collect { navController.handleNavigationCommand(it) }
     }
 
-    private val navigationCommands = MutableSharedFlow<NavigationCommand>(
-        extraBufferCapacity = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
-
-    fun navigate(route: String) {
-        navigationCommands.tryEmit(NavigationCommand.NavigateToRoute(route))
-    }
-
-    fun navigate(route: String, optionsBuilder: NavOptionsBuilder.() -> Unit) {
-        val options = navOptions(optionsBuilder)
+    fun navigate(route: String, optionsBuilder: (NavOptionsBuilder.() -> Unit)? = null) {
+        val options = optionsBuilder?.let { navOptions(it) }
         navigationCommands.tryEmit(NavigationCommand.NavigateToRoute(route, options))
     }
 
@@ -57,20 +39,18 @@ class NavigationManager @Inject constructor() {
         navigationCommands.tryEmit(NavigationCommand.PopUpToRoute(route, inclusive))
     }
 
-    fun <T> navigateBackWithResult(key: String, result: T, destination: String? = null) {
-        val navController = navControllerFlow.value ?: run {
-            // shouldn't happen
-            return
-        }
-        val backStackEntry = destination?.let { navController.getBackStackEntry(it) }
-            ?: navController.previousBackStackEntry
-        backStackEntry?.savedStateHandle?.set(key, result)
-
-        destination?.let {
-            navController.popBackStack(it, false)
-        } ?: run {
-            navController.navigateUp()
-        }
+    fun <T> navigateBackWithResult(
+        key: String,
+        result: T,
+        destination: String? = null
+    ) {
+        navigationCommands.tryEmit(
+            NavigationCommand.NavigateUpWithResult(
+                key = key,
+                result = result,
+                destination = destination
+            )
+        )
     }
 
     fun <T> observeResult(key: String, route: String? = null): Flow<T> {
@@ -91,12 +71,47 @@ class NavigationManager @Inject constructor() {
                 } ?: emptyFlow()
             }
     }
+
+    private fun NavController.handleNavigationCommand(navigationCommand: NavigationCommand) {
+        when (navigationCommand) {
+            is NavigationCommand.NavigateToRoute -> navigate(
+                navigationCommand.route,
+                navigationCommand.options
+            )
+            NavigationCommand.NavigateUp -> navigateUp()
+            is NavigationCommand.PopUpToRoute -> popBackStack(
+                navigationCommand.route,
+                navigationCommand.inclusive
+            )
+            is NavigationCommand.NavigateUpWithResult<*> -> {
+                val backStackEntry =
+                    navigationCommand.destination?.let { getBackStackEntry(it) }
+                        ?: previousBackStackEntry
+                backStackEntry?.savedStateHandle?.set(
+                    navigationCommand.key,
+                    navigationCommand.result
+                )
+
+                navigationCommand.destination?.let {
+                    popBackStack(it, false)
+                } ?: run {
+                    navigateUp()
+                }
+            }
+        }
+    }
 }
 
 sealed class NavigationCommand {
     object NavigateUp : NavigationCommand()
     data class NavigateToRoute(val route: String, val options: NavOptions? = null) :
         NavigationCommand()
+
+    data class NavigateUpWithResult<T>(
+        val key: String,
+        val result: T,
+        val destination: String? = null
+    ) : NavigationCommand()
 
     data class PopUpToRoute(val route: String, val inclusive: Boolean) : NavigationCommand()
 }
