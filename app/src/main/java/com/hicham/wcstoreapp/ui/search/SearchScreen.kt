2359e237c429521c0cd3411b229a6d8e1c690265
hicham <hicham.boushaba@gmail.com>
2021-10-10 19:56:11 +0100

    Improve UI of SearchScreen

--- app/src/main/java/com/hicham/wcstoreapp/ui/search/SearchScreen.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/search/SearchScreen.kt
@@ -1,16 +1,22 @@
 package com.hicham.wcstoreapp.ui.search
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.PagingData
 import com.hicham.wcstoreapp.models.Product
 import com.hicham.wcstoreapp.ui.products.ProductUiModel
 import com.hicham.wcstoreapp.ui.products.ProductsList
+import compose.icons.TablerIcons
+import compose.icons.tablericons.Search
+import compose.icons.tablericons.X
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 @Composable
 fun SearchScreen(viewModel: SearchViewModel, scaffoldState: ScaffoldState) {
@@ -33,8 +39,43 @@ fun SearchScreen(
     removeItemFromCart: (Product) -> Unit = {},
     openProduct: (Long) -> Unit = {}
 ) {
-    Column {
-        TextField(value = searchQuery, onValueChange = onQueryChanged)
+    Scaffold(scaffoldState = scaffoldState, topBar = {
+        TopAppBar {
+            TextField(
+                value = searchQuery,
+                onValueChange = onQueryChanged,
+                leadingIcon = {
+                    Icon(
+                        imageVector = TablerIcons.Search,
+                        contentDescription = "",
+                        tint = MaterialTheme.colors.onPrimary
+                    )
+                },
+                trailingIcon = {
+                    if (searchQuery.isNotEmpty()) {
+                        IconButton(
+                            onClick = {
+                                onQueryChanged("")
+                            }
+                        ) {
+                            Icon(
+                                TablerIcons.X,
+                                contentDescription = "",
+                                tint = MaterialTheme.colors.onPrimary
+                            )
+                        }
+                    }
+                },
+                singleLine = true,
+                shape = RectangleShape,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = MaterialTheme.colors.primary,
+                    cursorColor = MaterialTheme.colors.onPrimary
+                )
+            )
+        }
+    }) {
 
         ProductsList(
             productsFlow = productsList,
@@ -44,4 +85,14 @@ fun SearchScreen(
             scaffoldState = scaffoldState
         )
     }
+}
+
+@Composable
+@Preview
+private fun SearchPreview() {
+    SearchScreen(
+        productsList = emptyFlow(),
+        searchQuery = "test",
+        scaffoldState = rememberScaffoldState()
+    )
 }
