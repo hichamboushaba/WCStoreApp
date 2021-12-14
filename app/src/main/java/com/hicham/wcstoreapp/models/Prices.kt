107b6b0f9fee38cd93681041f5715b0130028779
hicham <hicham.boushaba@gmail.com>
2021-12-14 11:33:55 +0100

    Improve formatting to match site's settings

--- app/src/main/java/com/hicham/wcstoreapp/models/Prices.kt
+++ app/src/main/java/com/hicham/wcstoreapp/models/Prices.kt
@@ -2,6 +2,7 @@ package com.hicham.wcstoreapp.models
 
 import com.hicham.wcstoreapp.data.api.NetworkPrices
 import java.math.BigDecimal
+import java.text.DecimalFormat
 
 data class Prices(
     val price: BigDecimal,
@@ -13,13 +14,32 @@ data class Prices(
 )
 
 fun NetworkPrices.toDomainModel(): Prices {
-    fun format(price: BigDecimal): String {
-        return "$currencyPrefix${price.toPlainString()}$currencySuffix"
+    val decimalFormat = if (currencyMinorUnit > 0) {
+        DecimalFormat("#,##0.${"0".repeat(currencyMinorUnit)}")
+    } else {
+        DecimalFormat("#,##0")
     }
 
-    val price = price.divide(BigDecimal.TEN.pow(currencyMinorUnit))
-    val regularPrice = regularPrice.divide(BigDecimal.TEN.pow(currencyMinorUnit))
-    val salePrice = salePrice.divide(BigDecimal.TEN.pow(currencyMinorUnit))
+    decimalFormat.decimalFormatSymbols = decimalFormat.decimalFormatSymbols.apply {
+        // If no decimal separator is set, keep whatever the system default is
+        currencyDecimalSeparator.takeIf { it.isNotEmpty() }?.let {
+            decimalSeparator = it[0]
+        }
+        // If no thousands separator is set, assume it's intentional and clear it in the formatter
+        currencyThousandSeparator.takeIf { it.isNotEmpty() }?.let {
+            groupingSeparator = it[0]
+        } ?: run { decimalFormat.isGroupingUsed = false }
+    }
+
+    fun convert(price: BigDecimal): BigDecimal = price.divide(BigDecimal.TEN.pow(currencyMinorUnit))
+    fun format(price: BigDecimal): String {
+        return "$currencyPrefix${decimalFormat.format(price)}$currencySuffix"
+    }
+
+    val price = convert(price)
+    val regularPrice = convert(regularPrice)
+    val salePrice = convert(salePrice)
+
     return Prices(
         price = price,
         regularPrice = regularPrice,
