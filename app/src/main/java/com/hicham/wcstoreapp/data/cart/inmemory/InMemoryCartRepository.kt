88c18d154440d94935a76f25bd9c4d7c63cb7faf
hicham <hicham.boushaba@gmail.com>
2021-12-15 12:33:38 +0100

    Update in-memory cart implementation

--- app/src/main/java/com/hicham/wcstoreapp/data/cart/inmemory/InMemoryCartRepository.kt
+++ app/src/main/java/com/hicham/wcstoreapp/data/cart/inmemory/InMemoryCartRepository.kt
@@ -5,31 +5,31 @@ import com.hicham.wcstoreapp.models.Cart
 import com.hicham.wcstoreapp.models.CartItem
 import com.hicham.wcstoreapp.models.CartTotals
 import com.hicham.wcstoreapp.models.Product
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import java.math.BigDecimal
 import javax.inject.Inject
 
 class InMemoryCartRepository @Inject constructor() : CartRepository {
-    private val _cart =
-        MutableStateFlow(
-            Cart(
-                emptyList(),
-                totals = CartTotals(
-                    BigDecimal.ZERO,
-                    BigDecimal.ZERO,
-                    BigDecimal.ZERO,
-                    BigDecimal.ZERO
-                )
+    private val _items = MutableStateFlow(emptyList<CartItem>())
+    override val cart: Flow<Cart> = _items.map {
+        val totalPrice = it.sumOf { it.product.prices.price }
+        Cart(
+            items = it,
+            totals = CartTotals(
+                subtotal = totalPrice,
+                tax = BigDecimal.ZERO,
+                shippingEstimate = null,
+                total = totalPrice
             )
         )
-    override val cart: StateFlow<Cart> = _cart.asStateFlow()
+    }
 
     override suspend fun addItem(product: Product): Result<Unit> {
-        _cart.update { cart ->
-            cart.copy(items = cart.items.toMutableList().apply {
+        _items.update { list ->
+            list.toMutableList().apply {
                 val index = indexOfFirst { it.product == product }
                 if (index == -1) {
                     add(CartItem(id = "", product, 1))
@@ -37,40 +37,29 @@ class InMemoryCartRepository @Inject constructor() : CartRepository {
                     val currentItem = get(index)
                     set(index, currentItem.copy(quantity = currentItem.quantity + 1))
                 }
-            })
+            }
         }
         return Result.success(Unit)
     }
 
     override suspend fun deleteItem(product: Product): Result<Unit> {
-        _cart.update { cart ->
-            cart.copy(items = cart.items
+        _items.update { list ->
+            list
                 .map {
                     if (it.product == product) it.copy(quantity = it.quantity - 1) else it
                 }
                 .filter { it.quantity > 0 }
-            )
         }
         return Result.success(Unit)
     }
 
     override suspend fun clearProduct(product: Product): Result<Unit> {
-        _cart.update { cart ->
-            cart.copy(items = cart.items.filterNot { it.product == product })
-        }
+        _items.update { list -> list.filterNot { it.product == product } }
         return Result.success(Unit)
     }
 
     override suspend fun clear(): Result<Unit> {
-        _cart.value = Cart(
-            emptyList(),
-            totals = CartTotals(
-                BigDecimal.ZERO,
-                BigDecimal.ZERO,
-                BigDecimal.ZERO,
-                BigDecimal.ZERO
-            )
-        )
+        _items.value = emptyList()
         return Result.success(Unit)
     }
 }
