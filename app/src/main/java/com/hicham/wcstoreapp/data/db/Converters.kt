2f53c850b5dc12ea21953f67b0f443ebc18223b0
hicham <hicham.boushaba@gmail.com>
2021-12-15 15:43:33 +0100

    Handle null values for the conversion

--- app/src/main/java/com/hicham/wcstoreapp/data/db/Converters.kt
+++ app/src/main/java/com/hicham/wcstoreapp/data/db/Converters.kt
@@ -4,7 +4,6 @@ import androidx.room.TypeConverter
 import java.math.BigDecimal
 
 class Converters {
-
     @TypeConverter
     fun listToString(value: List<String>) = value.joinToString(";")
 
@@ -12,8 +11,8 @@ class Converters {
     fun stringToList(value: String) = value.split(";")
 
     @TypeConverter
-    fun bigDecimalToString(value: BigDecimal) = value.toPlainString()
+    fun bigDecimalToString(value: BigDecimal?) = value?.toPlainString()
 
     @TypeConverter
-    fun stringToBigDecimal(value: String) = BigDecimal(value)
+    fun stringToBigDecimal(value: String?) = value?.let { BigDecimal(it) }
 }
