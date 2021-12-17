5e2c5686b1952042b661c02223ea219bcf0c0b99
hicham <hicham.boushaba@gmail.com>
2021-12-17 23:23:51 +0100

    Force light mode

--- app/src/main/java/com/hicham/wcstoreapp/ui/theme/Theme.kt
+++ app/src/main/java/com/hicham/wcstoreapp/ui/theme/Theme.kt
@@ -29,11 +29,14 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun WCStoreAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+//    TODO fix dark theme
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
