7068d3905a4c79b2ffada1872cf09581d3973261
hicham <hicham.boushaba@gmail.com>
2022-03-07 14:59:40 +0100

    Set base host path config for ktor

--- app/src/main/java/com/hicham/wcstoreapp/di/NetworkModule.kt
+++ app/src/main/java/com/hicham/wcstoreapp/di/NetworkModule.kt
@@ -4,6 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import com.hicham.wcstoreapp.BuildConfig
 import com.hicham.wcstoreapp.data.api.WooCommerceApi
+import com.hicham.wcstoreapp.data.api.WooCommerceApiKtorImpl
 import com.hicham.wcstoreapp.util.DataStoreCookieJar
 import com.hicham.wcstoreapp.util.NonceInterceptor
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
@@ -12,7 +13,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.*
+import io.ktor.client.features.*
 import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.json.Json
 import logcat.LogPriority
@@ -21,6 +25,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.net.URL
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -70,9 +75,20 @@ abstract class NetworkModule {
         @Provides
         @Singleton
         fun providesKtorClient(): HttpClient {
+            val baseUrl = URL(BuildConfig.WC_URL)
             return HttpClient {
+                defaultRequest {
+                    host = baseUrl.host
+                    url {
+                        protocol = URLProtocol.byName[baseUrl.protocol]!!
+                    }
+                }
                 install(Logging) {
                     level = if (BuildConfig.DEBUG) LogLevel.ALL else LogLevel.NONE
+                }
+
+                HttpResponseValidator {
+                    handleResponseException {  }
                 }
             }
         }
