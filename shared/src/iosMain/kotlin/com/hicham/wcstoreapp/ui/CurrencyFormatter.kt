1f2155fb895317dee740f0507beaf37e7b674d00
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-09-16 18:04:45 +0100

    Add a currency formatter for iOS

--- shared/src/iosMain/kotlin/com/hicham/wcstoreapp/ui/CurrencyFormatter.kt
+++ shared/src/iosMain/kotlin/com/hicham/wcstoreapp/ui/CurrencyFormatter.kt
@@ -2,29 +2,23 @@ package com.hicham.wcstoreapp.ui
 
 import com.hicham.wcstoreapp.models.CurrencyFormatSettings
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import kotlinx.cinterop.convert
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
+import platform.darwin.NSUInteger
 
 actual class CurrencyFormatter actual constructor(private val currencyFormatSettings: CurrencyFormatSettings) {
     actual fun format(price: BigDecimal): String = with(currencyFormatSettings) {
-//        val price = java.math.BigDecimal(price.toString())
-//        val decimalFormat = if (currencyDecimalNumber > 0) {
-//            DecimalFormat("#,##0.${"0".repeat(currencyDecimalNumber)}")
-//        } else {
-//            DecimalFormat("#,##0")
-//        }
-//
-//        decimalFormat.decimalFormatSymbols = decimalFormat.decimalFormatSymbols.apply {
-//            // If no decimal separator is set, keep whatever the system default is
-//            currencyDecimalSeparator.takeIf { it.isNotEmpty() }?.let {
-//                decimalSeparator = it[0]
-//            }
-//            // If no thousands separator is set, assume it's intentional and clear it in the formatter
-//            currencyThousandSeparator.takeIf { it.isNotEmpty() }?.let {
-//                groupingSeparator = it[0]
-//            } ?: run { decimalFormat.isGroupingUsed = false }
-//        }
-//
-//        return "$currencyPrefix${decimalFormat.format(price)}$currencySuffix"
-        // TODO
-        return@with price.toPlainString()
+        val price = NSNumberFormatter().numberFromString(price.toPlainString())!!
+
+        val numberFormatter = NSNumberFormatter().apply {
+            decimalSeparator = currencyDecimalSeparator
+            groupingSeparator = currencyGroupingSeparator
+            usesGroupingSeparator = currencyGroupingSeparator.isNotEmpty()
+            this.maximumFractionDigits = currencyDecimalNumber.toUInt().convert()
+            this.minimumFractionDigits = currencyDecimalNumber.toUInt().convert()
+        }
+
+        return "$currencyPrefix${numberFormatter.stringFromNumber(price)}$currencySuffix"
     }
 }
