68981c98a00c48ae0905b939f42d0eccefb30b40
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-12-07 19:43:22 +0100

    Handle network errors in iOS Ktor engine

--- shared/src/commonMain/kotlin/com/hicham/wcstoreapp/util/NetworkErrorsMapper.kt
+++ shared/src/commonMain/kotlin/com/hicham/wcstoreapp/util/NetworkErrorsMapper.kt
@@ -1,6 +1,7 @@
 package com.hicham.wcstoreapp.util
 
 import io.ktor.client.features.*
+import io.ktor.utils.io.errors.*
 
 suspend fun <T> Any.runCatchingNetworkErrors(block: suspend () -> T): Result<T> {
     return try {
@@ -10,6 +11,9 @@ suspend fun <T> Any.runCatchingNetworkErrors(block: suspend () -> T): Result<T> 
         log(LogPriority.WARN) { e.stackTraceToString() }
         Result.failure(e)
     } catch (e: KtorNetworkException) {
+        log(LogPriority.WARN) { e.stackTraceToString() }
+        Result.failure(e)
+    } catch (e: IOException) {
         log(LogPriority.WARN) { e.stackTraceToString() }
         Result.failure(e)
     }
--- shared/src/iosMain/kotlin/com/hicham/wcstoreapp/di/KtorModule.kt
+++ shared/src/iosMain/kotlin/com/hicham/wcstoreapp/di/KtorModule.kt
@@ -1,8 +1,9 @@
 package com.hicham.wcstoreapp.di
 
 import com.hicham.wcstoreapp.BuildKonfig
-import com.hicham.wcstoreapp.util.KtorUserDefaultsCookiesStorage
+import com.hicham.wcstoreapp.util.KtorNetworkException
 import com.hicham.wcstoreapp.util.KtorNoncePlugin
+import com.hicham.wcstoreapp.util.KtorUserDefaultsCookiesStorage
 import com.hicham.wcstoreapp.util.log
 import io.ktor.client.*
 import io.ktor.client.engine.ios.*
@@ -42,6 +43,20 @@ actual fun Module.ktor() {
             install(KtorNoncePlugin())
             install(HttpCookies) {
                 storage = KtorUserDefaultsCookiesStorage()
+            }
+            HttpResponseValidator {
+                handleResponseException {
+                    when (it) {
+                        is ResponseException -> throw it
+                        else -> {
+                            // Ktor seems to throw whatever exceptions the engine throws in case
+                            // of network connections, let's map it to a wrapper exception to be
+                            // able to catch it without a broad [Throwable] catch block.
+                            // TODO remove this when this is fixed https://youtrack.jetbrains.com/issue/KTOR-2630
+                            throw KtorNetworkException(it)
+                        }
+                    }
+                }
             }
             if (Platform.isDebugBinary) install(Logging) {
                 level = LogLevel.ALL
